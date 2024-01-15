7f31701db567fc2a95005f8f5fe90f5ea20c776b
Nathan Rogers <nrogers.careers@gmail.com>
2024-01-15 13:53:09 -0800

    leetcode accepted answer

--- 01-two-sum/twosum.go
+++ 01-two-sum/twosum.go
@@ -1,3 +1,21 @@
 package twosum
 
-//placehold code here
+func TwoSum(nums []int, target int) []int {
+	var solution []int
+
+	m := make(map[int]int)
+
+	for i, v := range nums {
+		x := target - v
+
+		_, ok := m[x]
+		if ok {
+			solution = append(solution, m[x], i)
+			break
+		} else {
+			m[v] = i
+		}
+	}
+
+	return solution
+}
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	twosum "github.com/Nathan-G-Rogers/golang-leetcode-learning/01-two-sum"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	solution := twosum.TwoSum([]int{2, 9, 11, 20}, 29)
+
+	fmt.Println(solution)
 }

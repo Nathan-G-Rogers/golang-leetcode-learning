23619f5241a4e821b1f82884d751f73bb2272067
Nathan Rogers <nrogers.careers@gmail.com>
2024-01-16 21:31:00 -0800

    add two numbers answer accepted

---
+++ 02-add-two-numbers/addtwonumbers.go
@@ -0,0 +1,26 @@
+package addtwonumbers
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3, sum := new(ListNode), 0
+
+	for val := l3; l1 != nil || l2 != nil || sum != 0; {
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		val.Next = &ListNode{Val: sum % 10}
+		sum /= 10
+		val = val.Next
+	}
+
+	return l3.Next
+}
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	twosum "github.com/Nathan-G-Rogers/golang-leetcode-learning/01-two-sum"
+	addtwonumbers "github.com/Nathan-G-Rogers/golang-leetcode-learning/02-add-two-numbers"
 )
 
 func main() {
@@ -12,4 +13,13 @@ func main() {
 	solution := twosum.TwoSum([]int{2, 9, 11, 20}, 29)
 
 	fmt.Println(solution)
+
+	l1 := &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, nil}}}}}}
+	l2 := &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, &addtwonumbers.ListNode{9, nil}}}}}
+
+	solution2 := addtwonumbers.AddTwoNumbers(l1, l2)
+
+	for node := solution2; node != nil; node = node.Next {
+		fmt.Print(node.Val)
+	}
 }
